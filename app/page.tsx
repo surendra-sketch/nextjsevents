48384ec51b3e7993de9a5110290c2fc2c676816e
surendra-sketch <surendraji2525@gmail.com>
2024-03-07 23:27:45 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="destructive" className="px-5">
+        Hello
+      </Button>
+    </main>
+  );
+}
